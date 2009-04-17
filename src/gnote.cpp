3d09e5bf33c527b9737949f65a8577764326f623
Robert Millan <rmh@aybabtu.com>
2009-04-17 17:45:50 -0400

    * Some strings shouldn't be translated. (Close 579197)
    Signed-off-by: Hubert Figuiere <hub@figuiere.net>

--- src/gnote.cpp
+++ src/gnote.cpp
@@ -259,7 +259,7 @@ namespace gnote {
     about.set_name("Gnote");
     about.set_version(VERSION);
     about.set_logo(utils::get_icon("gnote", 48));
-    about.set_copyright(_("Copyright (c) 2009 Hubert Figuiere"));
+    about.set_copyright("Copyright (c) 2009 Hubert Figuiere");
     about.set_comments(_("A simple and easy to use desktop "
                          "note-taking application."));
 // I don't think we need a hook.
