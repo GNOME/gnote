dc3001abd1cb301096773c5d25a7a05f475eb2f8
Aurimas Černius <aurisc4@gmail.com>
2011-05-08 00:26:49 +0300

    Change GDK_U to GDK_KEY_U in underlinemenuitem.cpp
    The former is not available in GTK 3.

--- src/addins/underline/underlinemenuitem.cpp
+++ src/addins/underline/underlinemenuitem.cpp
@@ -1,7 +1,7 @@
 /*
  * gnote
  *
- * Copyright (C) 2010 Aurimas Cernius
+ * Copyright (C) 2010-2011 Aurimas Cernius
  * Copyright (C) 2009 Hubert Figuiere
  *
  * This program is free software: you can redistribute it and/or modify
@@ -42,7 +42,7 @@ namespace underline {
 
     add_accelerator ("activate",
                      addin->get_window()->get_accel_group(),
-                     GDK_U,
+                     GDK_KEY_U,
                      Gdk::CONTROL_MASK,
                      Gtk::ACCEL_VISIBLE);
 
