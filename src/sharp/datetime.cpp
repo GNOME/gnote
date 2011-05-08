83289da65ab277e4d3b6880ac91f93b7efda5cfb
Aurimas Černius <aurisc4@gmail.com>
2011-05-08 16:14:20 +0300

    Pass const argument to localtime_r
    Passing non-const breaks build on FreeBSD and OpenBSD.
    Thanks Jasper Lievisse Adriaanse.
    Fixes bug 649472.

--- src/sharp/datetime.cpp
+++ src/sharp/datetime.cpp
@@ -70,28 +70,28 @@ namespace sharp {
   int DateTime::day() const
   {
     struct tm result;
-    localtime_r(&m_date.tv_sec, &result);
+    localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result);
     return result.tm_mday;
   }
 
   int DateTime::month() const
   {
     struct tm result;
-    localtime_r(&m_date.tv_sec, &result);
+    localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result);
     return result.tm_mon + 1;
   }
 
   int DateTime::year() const
   {
     struct tm result;
-    localtime_r(&m_date.tv_sec, &result);
+    localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result);
     return result.tm_year + 1900;
   }
 
   int DateTime::day_of_year() const
   {
     struct tm result;
-    localtime_r(&m_date.tv_sec, &result);
+    localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result);
     return result.tm_yday;
   }
 
@@ -110,14 +110,14 @@ namespace sharp {
   std::string DateTime::to_string(const char * format) const
   {
     struct tm result; 
-    return _to_string(format, localtime_r(&m_date.tv_sec, &result));
+    return _to_string(format, localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result));
   }
 
 
   std::string DateTime::to_short_time_string() const
   {
     struct tm result;
-    return _to_string("%R", localtime_r(&m_date.tv_sec, &result));
+    return _to_string("%R", localtime_r(const_cast<const time_t *>(&m_date.tv_sec), &result));
   }
 
   std::string DateTime::to_iso8601() const
