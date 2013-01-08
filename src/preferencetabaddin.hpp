e5f45dcb753413884d622c714fff131410f87fa9
Aurimas Černius <aurisc4@gmail.com>
2013-01-08 22:18:27 +0200

    Make Gtk::Dialog first argument of get_preference_tab_widget
    Avoid dependency on PreferencesDialog.
    Also fix indentation.

--- src/preferencetabaddin.hpp
+++ src/preferencetabaddin.hpp
@@ -1,6 +1,7 @@
 /*
  * gnote
  *
+ * Copyright (C) 2013 Aurimas Cernius
  * Copyright (C) 2009 Hubert Figuiere
  *
  * This program is free software: you can redistribute it and/or modify
@@ -32,35 +33,35 @@
 
 namespace gnote {
 
-	/// Implement this interface to provide a new tab in
-	/// Tomboy's Preferences Dialog.  If you are writing
-	/// a standard add-in, DO NOT ABUSE THIS (you should
-	/// normally extend the /Tomboy/AddinPreferences
-	/// extension point).
-	class PreferenceTabAddin 
+  /// Implement this interface to provide a new tab in
+  /// Tomboy's Preferences Dialog.  If you are writing
+  /// a standard add-in, DO NOT ABUSE THIS (you should
+  /// normally extend the /Tomboy/AddinPreferences
+  /// extension point).
+  class PreferenceTabAddin 
     : public AbstractAddin
-	{
-		/// <summary>
-		/// Returns a Gtk.Widget to place in a new tab in Tomboy's
-		/// preferences dialog.
-		/// <param name="parent">The preferences dialog.  Add-ins should
-		/// use this for connecting to Hidden or other events as needed.
-		/// Another use would be to pop open dialogs, so they can properly
-		/// set their parent.
-		/// </param>
-		/// <param name="tabLabel">The string to be used in the tab's
-		/// label.</param>
-		/// <param name="preferenceWidget">The Gtk.Widget to use as the
-		/// content of the tab page.</param>
-		/// <returns>Returns <value>true</value> if the widget is
-		/// valid/created or <value>false</value> otherwise.</returns>
-		/// </summary>
+  {
+    /// <summary>
+    /// Returns a Gtk.Widget to place in a new tab in Tomboy's
+    /// preferences dialog.
+    /// <param name="parent">The preferences dialog.  Add-ins should
+    /// use this for connecting to Hidden or other events as needed.
+    /// Another use would be to pop open dialogs, so they can properly
+    /// set their parent.
+    /// </param>
+    /// <param name="tabLabel">The string to be used in the tab's
+    /// label.</param>
+    /// <param name="preferenceWidget">The Gtk.Widget to use as the
+    /// content of the tab page.</param>
+    /// <returns>Returns <value>true</value> if the widget is
+    /// valid/created or <value>false</value> otherwise.</returns>
+    /// </summary>
   public:
     virtual bool get_preference_tab_widget (
-									PreferencesDialog * parent,
-									std::string  & tabLabel,
-									Gtk::Widget * preferenceWidget) = 0;
-	};
+                  Gtk::Dialog * parent,
+                  std::string  & tabLabel,
+                  Gtk::Widget * preferenceWidget) = 0;
+  };
 
 }
 
