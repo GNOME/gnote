86915257ab95752ffa9c0077f740b8abb5a26696
Aurimas Černius <aurisc4@gmail.com>
2013-08-04 22:49:40 +0300

    Add Special Notes notebook
    Notebook to display otherwise hidden notes.

---
+++ src/addins/specialnotes/specialnotesapplicationaddin.cpp
@@ -0,0 +1,109 @@
+/*
+ * gnote
+ *
+ * Copyright (C) 2013 Aurimas Cernius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <glibmm/i18n.h>
+
+#include "notebooks/notebook.hpp"
+#include "notebooks/notebookmanager.hpp"
+#include "specialnotesapplicationaddin.hpp"
+
+using gnote::Note;
+using gnote::notebooks::Notebook;
+
+
+namespace {
+  class SpecialNotesNotebook
+    : public gnote::notebooks::SpecialNotebook
+  {
+  public:
+    SpecialNotesNotebook(gnote::NoteManager & nm)
+      : SpecialNotebook(nm, _("Special Notes"))
+      {}
+
+    virtual std::string get_normalized_name() const override
+      {
+        return "___SpecialNotesAddin___SpecialNotes__Notebook___";
+      }
+
+    virtual bool contains_note(const Note::Ptr & note, bool = false) override
+      {
+        return is_template_note(note);
+      }
+
+    virtual bool add_note(const Note::Ptr &) override
+      {
+        return false;
+      }
+
+    /*virtual Glib::RefPtr<Gdk::Pixbuf> get_icon() override
+      {
+      }*/
+  };
+}
+
+
+namespace specialnotes {
+
+DECLARE_MODULE(SpecialNotesModule);
+
+
+SpecialNotesModule::SpecialNotesModule()
+{
+  ADD_INTERFACE_IMPL(SpecialNotesApplicationAddin);
+}
+
+
+const char *SpecialNotesApplicationAddin::IFACE_NAME = "gnote::ApplicationAddin";
+
+SpecialNotesApplicationAddin::SpecialNotesApplicationAddin()
+  : m_initialized(false)
+{
+}
+
+SpecialNotesApplicationAddin::~SpecialNotesApplicationAddin()
+{
+}
+
+void SpecialNotesApplicationAddin::initialize()
+{
+  if(!m_initialized) {
+    m_initialized = true;
+
+    m_notebook = Notebook::Ptr(new SpecialNotesNotebook(note_manager()));
+    gnote::notebooks::NotebookManager::obj().add_notebook(m_notebook);
+  }
+}
+
+void SpecialNotesApplicationAddin::shutdown()
+{
+  if(m_notebook != NULL) {
+    gnote::notebooks::NotebookManager::obj().delete_notebook(m_notebook);
+    m_notebook.reset();
+    m_initialized = false;
+  }
+}
+
+bool SpecialNotesApplicationAddin::initialized()
+{
+  return m_initialized;
+}
+
+}
+
---
+++ src/addins/specialnotes/specialnotesapplicationaddin.hpp
@@ -0,0 +1,62 @@
+/*
+ * gnote
+ *
+ * Copyright (C) 2013 Aurimas Cernius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SPECIAL_NOTES_APPLICATION_ADDIN_HPP_
+#define __SPECIAL_NOTES_APPLICATION_ADDIN_HPP_
+
+#include "base/macros.hpp"
+#include "notebooks/notebook.hpp"
+#include "sharp/dynamicmodule.hpp"
+#include "applicationaddin.hpp"
+
+namespace specialnotes {
+
+class SpecialNotesModule
+  : public sharp::DynamicModule
+{
+public:
+  SpecialNotesModule();
+};
+
+
+class SpecialNotesApplicationAddin
+  : public gnote::ApplicationAddin
+{
+public:
+  static const char * IFACE_NAME;
+
+  static SpecialNotesApplicationAddin *create()
+    {
+      return new SpecialNotesApplicationAddin;
+    }
+
+  virtual ~SpecialNotesApplicationAddin();
+  virtual void initialize() override;
+  virtual void shutdown() override;
+  virtual bool initialized() override;
+private:
+  SpecialNotesApplicationAddin();
+
+  bool m_initialized;
+  gnote::notebooks::Notebook::Ptr m_notebook;
+};
+
+}
+
+#endif
