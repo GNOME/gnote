22d321e9b49f4b19cd5caffdb776c2699cee280e
Aurimas Černius <aurisc4@gmail.com>
2015-02-07 19:13:15 +0200

    Update copyright

--- src/recentchanges.cpp
+++ src/recentchanges.cpp
@@ -1,7 +1,7 @@
 /*
  * gnote
  *
- * Copyright (C) 2010-2014 Aurimas Cernius
+ * Copyright (C) 2010-2015 Aurimas Cernius
  * Copyright (C) 2010 Debarshi Ray
  * Copyright (C) 2009 Hubert Figuiere
  *
