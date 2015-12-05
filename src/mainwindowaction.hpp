17ab12685342b52b6628fc73ed805cbb9a65346e
Aurimas Černius <aurisc4@gmail.com>
2015-12-05 17:13:55 +0200

    Add MainWindowAction class

---
+++ src/mainwindowaction.hpp
@@ -0,0 +1,44 @@
+/*
+ * gnote
+ *
+ * Copyright (C) 2015 Aurimas Cernius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _MAIN_WINDOW_ACTION_HPP_
+#define _MAIN_WINDOW_ACTION_HPP_
+
+#include <giomm/simpleaction.h>
+
+#include "base/macros.hpp"
+
+
+namespace gnote {
+
+class MainWindowAction
+  : public Gio::SimpleAction
+{
+public:
+  typedef Glib::RefPtr<MainWindowAction> Ptr;
+
+  static Ptr create(const Glib::ustring & name);
+protected:
+  MainWindowAction(const Glib::ustring & name);
+};
+
+}
+
+#endif
+
