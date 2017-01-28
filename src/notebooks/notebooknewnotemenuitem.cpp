80574534f275553c99b0831e94234863d517f7ce
Aurimas Černius <aurisc4@gmail.com>
2017-01-28 17:37:56 +0200

    Fix comment

--- src/notebooks/notebooknewnotemenuitem.cpp
+++ src/notebooks/notebooknewnotemenuitem.cpp
@@ -36,7 +36,7 @@ namespace gnote {
   namespace notebooks {
 
     NotebookNewNoteMenuItem::NotebookNewNoteMenuItem(const Notebook::Ptr & notebook)
-      // TRANSLATORS: %1%: boost format placeholder for the notebook name
+      // TRANSLATORS: %1: format placeholder for the notebook name
       : Gtk::ImageMenuItem(Glib::ustring::compose(_("New \"%1\" Note"), notebook->get_name()))
       , m_notebook(notebook)
     {
