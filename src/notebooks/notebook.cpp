7d8c724ad3ec84aca9f5adf28caf2b51d1a982bc
Aurimas Černius <aurisc4@gmail.com>
2019-04-07 18:35:02 +0300

    Remove FOREACH from notebook

--- src/notebooks/notebook.cpp
+++ src/notebooks/notebook.cpp
@@ -1,7 +1,7 @@
 /*
  * gnote
  *
- * Copyright (C) 2010-2014,2017 Aurimas Cernius
+ * Copyright (C) 2010-2014,2017,2019 Aurimas Cernius
  * Copyright (C) 2009 Hubert Figuiere
  *
  * This program is free software: you can redistribute it and/or modify
@@ -133,7 +133,7 @@ namespace notebooks {
     }
     std::list<NoteBase*> notes;
     templ_tag->get_notes(notes);
-    FOREACH(NoteBase *n, notes) {
+    for(NoteBase *n : notes) {
       if(n->contains_tag(notebook_tag)) {
         note = static_pointer_cast<Note>(n->shared_from_this());
         break;
--- src/notebooks/notebook.hpp
+++ src/notebooks/notebook.hpp
@@ -1,7 +1,7 @@
 /*
  * gnote
  *
- * Copyright (C) 2010-2014,2017 Aurimas Cernius
+ * Copyright (C) 2010-2014,2017,2019 Aurimas Cernius
  * Copyright (C) 2009 Hubert Figuiere
  *
  * This program is free software: you can redistribute it and/or modify
@@ -23,7 +23,6 @@
 #ifndef __NOTEBOOKS_NOTEBOOK_HPP_
 #define __NOTEBOOKS_NOTEBOOK_HPP_
 
-#include "base/macros.hpp"
 #include "tag.hpp"
 #include "note.hpp"
 
