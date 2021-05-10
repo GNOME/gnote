6973cd0bbca3f34135b4093357050b1fd32b8496
Aurimas Černius <aurisc4@gmail.com>
2021-05-10 23:03:15 +0300

    Replace deprecated margin_left with margin_start

--- src/notewindow.cpp
+++ src/notewindow.cpp
@@ -401,13 +401,13 @@ namespace gnote {
     text_button->set_image(*image);
     text_button->signal_clicked()
       .connect(sigc::mem_fun(*this, &NoteWindow::on_text_button_clicked));
-    text_button->property_margin_left() = 12;
+    text_button->property_margin_start() = 12;
     text_button->show_all();
     grid->attach(*text_button, grid_col++, 0, 1, 1);
     text_button->set_tooltip_text(_("Set properties of text"));
     m_text_menu->set_relative_to(*text_button);
 
-    grid->property_margin_left() = 12;
+    grid->property_margin_start() = 12;
     grid->show_all();
     return grid;
   }
