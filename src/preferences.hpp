d1f5afe36db1806443f039fea533fc1e50716daf
Aurimas Černius <aurisc4@gmail.com>
2021-10-24 17:36:03 +0300

    Remove close on escape setting

--- src/preferences.hpp
+++ src/preferences.hpp
@@ -64,7 +64,6 @@ namespace gnote {
     GNOTE_PREFERENCES_CACHING_SETTING(enable_wikiwords, bool)
     GNOTE_PREFERENCES_CACHING_SETTING(enable_custom_font, bool)
     GNOTE_PREFERENCES_SETTING_BOOL(enable_auto_bulleted_lists)
-    GNOTE_PREFERENCES_SETTING_BOOL(enable_close_note_on_escape)
     GNOTE_PREFERENCES_CACHING_SETTING(note_rename_behavior, int)
     GNOTE_PREFERENCES_CACHING_SETTING(open_notes_in_new_window, bool)
     GNOTE_PREFERENCES_SETTING_STRING(start_note_uri)
