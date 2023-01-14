bd5bf206cdc050ba24f8e045fcf04612e8138bba
Aurimas Černius <aurisc4@gmail.com>
2023-01-14 18:19:37 +0200

    Fix underline for gtk4

--- src/plugins/underline/underlinenoteaddin.cpp
+++ src/plugins/underline/underlinenoteaddin.cpp
@@ -1,7 +1,7 @@
 /*
  * gnote
  *
- * Copyright (C) 2010,2013,2016 Aurimas Cernius
+ * Copyright (C) 2010,2013,2016,2023 Aurimas Cernius
  * Copyright (C) 2009 Hubert Figuiere
  * Original C# file
  * (C) 2009 Mark Wakim <markwakim@gmail.com>
@@ -25,7 +25,9 @@
 
 
 #include <glibmm/i18n.h>
+#include <gtkmm/togglebutton.h>
 
+#include "debug.hpp"
 #include "sharp/modulefactory.hpp"
 #include "notewindow.hpp"
 #include "underlinenoteaddin.hpp"
@@ -42,45 +44,40 @@ namespace underline {
 
 
 
-  void UnderlineNoteAddin::initialize ()
+  void UnderlineNoteAddin::initialize()
   {
     // If a tag of this name already exists, don't install.
-    if (!get_note()->get_tag_table()->lookup ("underline")) {
-      m_tag = Glib::RefPtr<Gtk::TextTag>(new UnderlineTag ());
-				get_note()->get_tag_table()->add (m_tag);
-			}
-
-    Gtk::Widget *button = gnote::utils::create_popover_button("win.underline-enable", "");
-    auto label = dynamic_cast<Gtk::Label*>(dynamic_cast<Gtk::Bin*>(button)->get_child());
-    Glib::ustring lbl("<u>");
-    lbl += _("_Underline");
-    lbl += "</u>";
-    label->set_markup_with_mnemonic(lbl);
-    add_text_menu_item(button);
-  }
-
-
-  void UnderlineNoteAddin::shutdown ()
-  {
-	// Remove the tag only if we installed it.
-    if (m_tag) {
-      get_note()->get_tag_table()->remove (m_tag);
+    if(!get_note()->get_tag_table()->lookup("underline")) {
+      m_tag = Glib::make_refptr_for_instance(new UnderlineTag());
+      get_note()->get_tag_table()->add (m_tag);
     }
   }
 
 
-  void UnderlineNoteAddin::on_note_opened ()
+  void UnderlineNoteAddin::shutdown()
   {
-    get_window()->text_menu()->signal_show().connect(
-      sigc::mem_fun(*this, &UnderlineNoteAddin::menu_shown));
-    dynamic_cast<gnote::NoteTextMenu*>(get_window()->text_menu())->signal_set_accels
-      .connect(sigc::mem_fun(*this, &UnderlineNoteAddin::set_accels));
+    // Remove the tag only if we installed it.
+    if(m_tag) {
+      get_note()->get_tag_table()->remove(m_tag);
+      m_tag.reset();
+    }
+  }
 
+
+  void UnderlineNoteAddin::on_note_opened()
+  {
     gnote::NoteWindow *note_window = get_window();
     note_window->signal_foregrounded.connect(
       sigc::mem_fun(*this, &UnderlineNoteAddin::on_note_foregrounded));
     note_window->signal_backgrounded.connect(
       sigc::mem_fun(*this, &UnderlineNoteAddin::on_note_backgrounded));
+    note_window->signal_build_text_menu
+      .connect(sigc::mem_fun(*this, &UnderlineNoteAddin::add_menu_item));
+
+    auto trigger = Gtk::KeyvalTrigger::create(GDK_KEY_U, Gdk::ModifierType::CONTROL_MASK);
+    auto action = Gtk::NamedAction::create("win.underline-enable");
+    auto shortcut = Gtk::Shortcut::create(trigger, action);
+    note_window->shortcut_controller().add_shortcut(shortcut); // TODO also remove
   }
 
   void UnderlineNoteAddin::on_note_foregrounded()
@@ -94,6 +91,48 @@ namespace underline {
     m_on_underline_clicked_cid.disconnect();
   }
 
+  void UnderlineNoteAddin::add_menu_item(gnote::NoteTextMenu & menu)
+  {
+    auto box = dynamic_cast<Gtk::Box*>(menu.get_child());
+    if(!box) {
+      ERR_OUT("Menu child is not Gtk::Box");
+      return;
+    }
+
+    auto formatting = box->get_first_child();
+    while(formatting && formatting->get_name() != "formatting") {
+      formatting = formatting->get_next_sibling();
+    }
+    if(!formatting) {
+      ERR_OUT("Item 'formatting' not found");
+      return;
+    }
+    auto fmt_box = dynamic_cast<Gtk::Box*>(formatting);
+    if(!fmt_box) {
+      ERR_OUT("Item 'formatting' is not Gtk::Box");
+      return;
+    }
+    auto font_box = fmt_box->get_first_child();
+    while(font_box && font_box->get_name() != "font-box") {
+      font_box = font_box->get_next_sibling();
+    }
+    if(!font_box) {
+      ERR_OUT("Item 'font_box' not found");
+      return;
+    }
+    auto fnt_box = dynamic_cast<Gtk::Box*>(font_box);
+    if(!fnt_box) {
+     ERR_OUT("Item 'font-box' is not Gtk::Box");
+     return;
+    }
+
+    auto button = Gtk::make_managed<Gtk::ToggleButton>();
+    button->set_action_name("win.underline-enable");
+    button->set_icon_name("format-text-underline-symbolic");
+    button->set_has_frame(false);
+    fnt_box->append(*button);
+  }
+
   void UnderlineNoteAddin::on_underline_clicked(const Glib::VariantBase & state)
   {
     get_window()->host()->find_action("underline-enable")->set_state(state);
@@ -104,20 +143,6 @@ namespace underline {
   {
     get_buffer()->toggle_active_tag("underline");
   }
-
-  void UnderlineNoteAddin::menu_shown()
-  {
-    get_window()->host()->find_action("underline-enable")->set_state(
-      Glib::Variant<bool>::create(get_buffer()->is_active_tag("underline")));
-  }
-
-  void UnderlineNoteAddin::set_accels(const gnote::utils::GlobalKeybinder & keybinder)
-  {
-    const_cast<gnote::utils::GlobalKeybinder&>(keybinder).add_accelerator(
-      sigc::mem_fun(*this, &UnderlineNoteAddin::on_underline_pressed),
-      GDK_KEY_U, Gdk::CONTROL_MASK, Gtk::ACCEL_VISIBLE);
-  }
-
 
 }
 
