bbc04c0a5ec079b14290d9de71e48b89e18f5199
Aurimas Černius <aurisc4@gmail.com>
2023-10-10 23:19:14 +0300

    Define Ref for Note

--- src/note.hpp
+++ src/note.hpp
@@ -91,6 +91,7 @@ class Note
 public:
   typedef std::shared_ptr<Note> Ptr;
   typedef std::weak_ptr<Note> WeakPtr;
+  typedef std::optional<std::reference_wrapper<Note>> Ref;
   typedef std::vector<Ptr> List;
 
   ~Note();
