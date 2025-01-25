d8bd5d26b74251da0e97535796d4c603e1aeead9
Aurimas Černius <aurisc4@gmail.com>
2025-01-25 22:12:07 +0200

    Update synchronization tests to have less macro magic

--- src/test/unit/syncmanagerutests.cpp
+++ src/test/unit/syncmanagerutests.cpp
@@ -38,53 +38,66 @@ using namespace gnote;
 
 SUITE(SyncManagerTests)
 {
-  struct Fixture
+  class Synchronizer
   {
-    Glib::ustring tempdir1;
-    Glib::ustring tempdir2;
-    Glib::ustring notesdir1;
-    Glib::ustring notesdir2;
-    Glib::ustring syncdir;
-    Glib::ustring manifest1;
-    Glib::ustring manifest2;
-    test::Gnote gnote1;
-    test::Gnote gnote2;
-    std::unique_ptr<test::NoteManager> manager1;
-    std::unique_ptr<test::NoteManager> manager2;
-    std::unique_ptr<test::SyncManager> sync_manager1;
-    std::unique_ptr<test::SyncManager> sync_manager2;
-    std::vector<Glib::ustring> files;
-
-    Fixture()
+  public:
+    Synchronizer(const Glib::ustring &tempdir, const Glib::ustring &syncdir)
+      : m_tempdir(tempdir)
+      , m_notesdir(tempdir + "/notes")
+      , m_manifest(tempdir + "/manifest.xml")
     {
-      tempdir1 = make_temp_dir();
-      tempdir2 = make_temp_dir();
-      notesdir1 = tempdir1 + "/notes";
-      notesdir2 = tempdir2 + "/notes";
-      syncdir = tempdir1 + "/sync";
-      REQUIRE CHECK(g_mkdir(syncdir.c_str(), S_IRWXU) == 0);
-      manifest1 = tempdir1 + "/manifest.xml";
-      manifest2 = tempdir2 + "/manifest.xml";
-
-      manager1.reset(new test::NoteManager(notesdir1, gnote1));
-      gnote1.notebook_manager(&manager1->notebook_manager());
-      create_note(*manager1, "note1", "content1");
-      create_note(*manager1, "note2", "content2");
-      create_note(*manager1, "note3", "content3");
-
-      manager2.reset(new test::NoteManager(notesdir2, gnote2));
-      gnote2.notebook_manager(&manager2->notebook_manager());
-
-      sync_manager1.reset(new test::SyncManager(gnote1, *manager1, syncdir));
-      gnote1.sync_manager(sync_manager1.get());
-      sync_manager2.reset(new test::SyncManager(gnote2, *manager2, syncdir));
-      gnote2.sync_manager(sync_manager2.get());
+      m_note_manager.reset(new test::NoteManager(m_notesdir, m_gnote));
+      m_gnote.notebook_manager(&m_note_manager->notebook_manager());
+      m_sync_manager.reset(new test::SyncManager(m_gnote, *m_note_manager, syncdir));
+      m_gnote.sync_manager(m_sync_manager.get());
     }
 
-    ~Fixture()
+    test::NoteManager &note_manager()
     {
-      test::remove_dir(tempdir1);
-      test::remove_dir(tempdir2);
+      return *m_note_manager;
+    }
+
+    const Glib::ustring &notes_dir() const
+    {
+      return m_notesdir;
+    }
+
+    void perform_sync()
+    {
+      auto _ = m_sync_manager->get_client(m_manifest);
+      auto ui = gnote::sync::SilentUI::create(m_gnote, *m_note_manager);
+      m_sync_manager->perform_synchronization(ui);
+    }
+  private:
+    test::Gnote m_gnote;
+    std::unique_ptr<test::NoteManager> m_note_manager;
+    std::unique_ptr<test::SyncManager> m_sync_manager;
+    const Glib::ustring &m_tempdir;
+    const Glib::ustring m_notesdir;
+    const Glib::ustring m_manifest;
+  };
+
+  struct Fixture1
+  {
+    Glib::ustring tempdir;
+    Glib::ustring syncdir;
+    Synchronizer synchronizer;
+
+    Fixture1()
+      : tempdir(make_temp_dir())
+      , syncdir(tempdir + "/sync")
+      , synchronizer(tempdir, syncdir)
+    {
+      REQUIRE CHECK(g_mkdir(syncdir.c_str(), S_IRWXU) == 0);
+      auto &note_manager = synchronizer.note_manager();
+      create_note(note_manager, "note1", "content1");
+      create_note(note_manager, "note2", "content2");
+      create_note(note_manager, "note3", "content3");
+    }
+
+    ~Fixture1()
+    {
+      test::remove_dir(tempdir);
     }
 
     Glib::ustring make_temp_dir()
@@ -95,18 +108,23 @@ SUITE(SyncManagerTests)
       return temp_dir;
     }
 
-    Glib::ustring make_note_content(const Glib::ustring & title, const Glib::ustring & body)
+    static Glib::ustring make_note_content(const Glib::ustring &title, const Glib::ustring &body)
     {
       return Glib::ustring::compose("<note-content><note-title>%1</note-title>\n\n%2</note-content>", title, body);
     }
 
-    void create_note(test::NoteManager & manager, Glib::ustring && title, Glib::ustring && body)
+    static void create_note(test::NoteManager &manager, Glib::ustring &&title, Glib::ustring &&body)
     {
       auto content = make_note_content(title, body);
       manager.create(std::move(title), std::move(content)).save();
     }
 
-    bool find_note_in_files(const Glib::ustring & title)
+    static std::vector<Glib::ustring> get_notes_in_dir(const Glib::ustring &dir)
+    {
+      return sharp::directory_get_files_with_ext(dir, ".note");
+    }
+
+    bool find_note_in_files(const std::vector<Glib::ustring> &files, const Glib::ustring &title)
     {
       Glib::ustring content_search = Glib::ustring::compose("<note-title>%1</note-title>", title);
       for(auto file : files) {
@@ -119,194 +137,195 @@ SUITE(SyncManagerTests)
       return false;
     }
 
-    void get_notes_in_dir(const Glib::ustring & dir)
+    void update_note(test::NoteManager &manager, const Glib::ustring &title, const Glib::ustring &new_title, const Glib::ustring &new_content)
     {
-      files = sharp::directory_get_files_with_ext(dir, ".note");
+      auto & note = dynamic_cast<test::Note&>(manager.find(title).value().get());
+      note.set_xml_content(make_note_content(new_title, new_title));
+      note.set_change_type(gnote::CONTENT_CHANGED);
+      note.save();
     }
   };
 
-#define FIRST_SYNC(ignote, sync_manager, note_manager, manifest, client, ui) \
-  test::SyncClient & client = dynamic_cast<test::SyncClient&>(sync_manager->get_client(manifest)); \
-  (void)client; \
-  gnote::sync::SilentUI::Ptr ui = gnote::sync::SilentUI::create(ignote, *note_manager); \
-  sync_manager->perform_synchronization(ui);
-
-#define UPDATE_NOTE(manager, title, new_title, new_content) \
-  do { \
-    auto & note = dynamic_cast<test::Note&>(manager->find(title).value().get()); \
-    note.set_xml_content(make_note_content(new_title, new_title)); \
-    note.set_change_type(gnote::CONTENT_CHANGED); \
-    note.save(); \
-  } \
-  while(0)
-
-  TEST_FIXTURE(Fixture, clean_sync)
+  struct Fixture2
+    : Fixture1
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client, sync_ui)
+    Glib::ustring tempdir2;
+    Synchronizer synchronizer2;
+
+    Fixture2()
+      : tempdir2(make_temp_dir())
+      , synchronizer2(tempdir2, syncdir)
+    {
+    }
+
+    ~Fixture2()
+    {
+      test::remove_dir(tempdir2);
+    }
+  };
+
+  TEST_FIXTURE(Fixture1, clean_sync)
+  {
+    synchronizer.perform_sync();
 
     Glib::ustring syncednotesdir = syncdir + "/0/0";
     REQUIRE CHECK(sharp::directory_exists(syncednotesdir));
-    get_notes_in_dir(syncednotesdir);
+    auto files = get_notes_in_dir(syncednotesdir);
     REQUIRE CHECK_EQUAL(3, files.size());
 
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note2"));
-    CHECK(find_note_in_files("note3"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note2"));
+    CHECK(find_note_in_files(files, "note3"));
   }
 
-  TEST_FIXTURE(Fixture, first_sync_existing_store)
+  TEST_FIXTURE(Fixture2, first_sync_existing_store)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer.perform_sync();
+    synchronizer2.perform_sync();
 
-    get_notes_in_dir(notesdir2);
+    auto files = get_notes_in_dir(synchronizer2.notes_dir());
     REQUIRE CHECK_EQUAL(3, files.size());
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note2"));
-    CHECK(find_note_in_files("note3"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note2"));
+    CHECK(find_note_in_files(files, "note3"));
   }
 
-  TEST_FIXTURE(Fixture, merge_two_clients)
+  TEST_FIXTURE(Fixture2, merge_two_clients)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
+    synchronizer.perform_sync();
 
-    create_note(*manager2, "note4", "content4");
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    create_note(synchronizer2.note_manager(), "note4", "content4");
+    synchronizer2.perform_sync();
 
     Glib::ustring syncednotesdir = syncdir + "/0/1";
     REQUIRE CHECK(sharp::directory_exists(syncednotesdir));
-    get_notes_in_dir(syncednotesdir);
+    auto files = get_notes_in_dir(syncednotesdir);
     REQUIRE CHECK_EQUAL(1, files.size());
-    CHECK(find_note_in_files("note4"));
+    CHECK(find_note_in_files(files, "note4"));
   }
 
-  TEST_FIXTURE(Fixture, download_new_notes_from_server)
+  TEST_FIXTURE(Fixture2, download_new_notes_from_server)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer.perform_sync();
+    synchronizer2.perform_sync();
 
     // create new note and sync again
-    create_note(*manager2, "note4", "content4");
-    sync_manager2->perform_synchronization(sync_ui2);
+    create_note(synchronizer2.note_manager(), "note4", "content4");
+    synchronizer2.perform_sync();
 
     // check sync dir contents
     Glib::ustring syncednotesdir = syncdir + "/0";
     REQUIRE CHECK(sharp::directory_exists(syncednotesdir));
-    files = sharp::directory_get_directories(syncednotesdir);
+    auto files = sharp::directory_get_directories(syncednotesdir);
     CHECK_EQUAL(2, files.size());  // first time client2 only downloads
 
     // sync to first client
-    sync_manager1->perform_synchronization(sync_ui1);
-    get_notes_in_dir(notesdir1);
+    synchronizer.perform_sync();
+    files = get_notes_in_dir(synchronizer.notes_dir());
     CHECK_EQUAL(4, files.size()); // 3 original + 1 from other client
-    CHECK(find_note_in_files("note4"));
+    CHECK(find_note_in_files(files, "note4"));
   }
 
-  TEST_FIXTURE(Fixture, upload_note_update)
+  TEST_FIXTURE(Fixture2, upload_note_update)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
+    synchronizer.perform_sync();
 
     // update note and sync again
-    UPDATE_NOTE(manager1, "note2", "note4", "updated content");
-    sync_client1.reparse();
-    sync_manager1->perform_synchronization(sync_ui1);
+    update_note(synchronizer.note_manager(), "note2", "note4", "updated content");
+    synchronizer.perform_sync();
 
     // check sync dir contents
     Glib::ustring syncednotesdir = syncdir + "/0";
     REQUIRE CHECK(sharp::directory_exists(syncednotesdir));
-    files = sharp::directory_get_directories(syncednotesdir);
+    auto files = sharp::directory_get_directories(syncednotesdir);
     CHECK_EQUAL(2, files.size());
 
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer2.perform_sync();
 
-    get_notes_in_dir(notesdir2);
+    files = get_notes_in_dir(synchronizer2.notes_dir());
     REQUIRE CHECK_EQUAL(3, files.size());
-    CHECK(!find_note_in_files("note2"));
-    CHECK(find_note_in_files("note4"));
+    CHECK(!find_note_in_files(files, "note2"));
+    CHECK(find_note_in_files(files, "note4"));
   }
 
-  TEST_FIXTURE(Fixture, download_note_update)
+  TEST_FIXTURE(Fixture2, download_note_update)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer.perform_sync();
+    synchronizer2.perform_sync();
 
     // update note and sync again
-    UPDATE_NOTE(manager1, "note2", "note4", "updated content");
-    sync_client1.reparse();
-    sync_manager1->perform_synchronization(sync_ui1);
+    update_note(synchronizer.note_manager(), "note2", "note4", "updated content");
+    synchronizer.perform_sync();
 
     // download updates
-    sync_manager2->perform_synchronization(sync_ui2);
-    get_notes_in_dir(notesdir2);
+    synchronizer2.perform_sync();
+    auto files = get_notes_in_dir(synchronizer2.notes_dir());
     REQUIRE CHECK_EQUAL(3, files.size());
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note3"));
-    CHECK(find_note_in_files("note4"));
-    CHECK(!find_note_in_files("note2"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note3"));
+    CHECK(find_note_in_files(files, "note4"));
+    CHECK(!find_note_in_files(files, "note2"));
   }
 
-  TEST_FIXTURE(Fixture, delete_note)
+  TEST_FIXTURE(Fixture2, delete_note)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
+    synchronizer.perform_sync();
 
     // remove note
-    auto note2 = manager1->find("note2");
+    auto note2 = synchronizer.note_manager().find("note2");
     REQUIRE CHECK(bool(note2));
-    manager1->delete_note(note2.value());
-    sync_manager1->perform_synchronization(sync_ui1);
+    synchronizer.note_manager().delete_note(note2.value());
+    synchronizer.perform_sync();
 
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
-    get_notes_in_dir(notesdir2);
+    synchronizer2.perform_sync();
+    auto files = get_notes_in_dir(synchronizer2.notes_dir());
     REQUIRE CHECK_EQUAL(2, files.size());
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note3"));
-    CHECK(!find_note_in_files("note2"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note3"));
+    CHECK(!find_note_in_files(files, "note2"));
   }
 
-  TEST_FIXTURE(Fixture, note_modification_conflict)
+  TEST_FIXTURE(Fixture2, note_modification_conflict)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer.perform_sync();
+    synchronizer2.perform_sync();
 
     // update note and sync again
-    UPDATE_NOTE(manager1, "note2", "note4", "updated content");
-    sync_client1.reparse();
-    sync_manager1->perform_synchronization(sync_ui1);
+    update_note(synchronizer.note_manager(), "note2", "note4", "updated content");
+    synchronizer.perform_sync();
 
     // update other client and sync
-    UPDATE_NOTE(manager2, "note2", "note5", "content updated");
-    sync_client2.reparse();
-    sync_manager2->perform_synchronization(sync_ui1);
+    update_note(synchronizer2.note_manager(), "note2", "note5", "content updated");
+    synchronizer2.perform_sync();
 
     // sync client1 again
-    sync_manager1->perform_synchronization(sync_ui1);
-    get_notes_in_dir(notesdir2);
+    synchronizer.perform_sync();
+    auto files = get_notes_in_dir(synchronizer2.notes_dir());
     REQUIRE CHECK_EQUAL(3, files.size());
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note3"));
-    CHECK(!find_note_in_files("note2"));
-    CHECK(find_note_in_files("note4"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note3"));
+    CHECK(!find_note_in_files(files, "note2"));
+    CHECK(find_note_in_files(files, "note4"));
   }
 
-  TEST_FIXTURE(Fixture, conflict_with_deletion_on_server)
+  TEST_FIXTURE(Fixture2, conflict_with_deletion_on_server)
   {
-    FIRST_SYNC(gnote1, sync_manager1, manager1, manifest1, sync_client1, sync_ui1)
-    FIRST_SYNC(gnote2, sync_manager2, manager2, manifest2, sync_client2, sync_ui2)
+    synchronizer.perform_sync();
+    synchronizer2.perform_sync();
 
     // remove note
-    auto & note2 = dynamic_cast<test::Note&>(manager2->find("note2").value().get());
-    manager2->delete_note(note2);
-    sync_manager2->perform_synchronization(sync_ui2);
+    auto & note2 = dynamic_cast<test::Note&>(synchronizer2.note_manager().find("note2").value().get());
+    synchronizer2.note_manager().delete_note(note2);
+    synchronizer2.perform_sync();
 
     // update note and sync again
-    UPDATE_NOTE(manager1, "note2", "note4", "updated content");
-    sync_client1.reparse();
-    sync_manager1->perform_synchronization(sync_ui1);
-    get_notes_in_dir(notesdir1);
+    update_note(synchronizer.note_manager(), "note2", "note4", "updated content");
+    synchronizer.perform_sync();
+    auto files = get_notes_in_dir(synchronizer.notes_dir());
     REQUIRE CHECK_EQUAL(2, files.size());
-    CHECK(find_note_in_files("note1"));
-    CHECK(find_note_in_files("note3"));
-    CHECK(!find_note_in_files("note2"));
+    CHECK(find_note_in_files(files, "note1"));
+    CHECK(find_note_in_files(files, "note3"));
+    CHECK(!find_note_in_files(files, "note2"));
   }
 }
 
