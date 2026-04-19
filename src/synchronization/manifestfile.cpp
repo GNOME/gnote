88e786c846f590099b7cd87777da9bc5da190d08
Aurimas Černius <aurimas.cernius@mailo.com>
2026-04-19 19:09:30 +0300

    Add wrapper for manifest file

---
+++ src/synchronization/manifestfile.cpp
@@ -0,0 +1,37 @@
+/*
+ * gnote
+ *
+ * Copyright (C) 2026 Aurimas Cernius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "manifestfile.hpp"
+
+
+namespace gnote {
+namespace sync {
+
+ManifestFile::ManifestFile(Glib::RefPtr<Gio::File> && path)
+  : m_path(std::move(path))
+{
+  if(!m_path) {
+    throw std::invalid_argument("Manifest path must be provided");
+  }
+}
+
+}
+}
+
---
+++ src/synchronization/manifestfile.hpp
@@ -0,0 +1,48 @@
+/*
+ * gnote
+ *
+ * Copyright (C) 2026 Aurimas Cernius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <giomm/file.h>
+
+
+namespace gnote {
+namespace sync {
+
+class ManifestFile
+{
+public:
+  ManifestFile(Glib::RefPtr<Gio::File> && path);
+  ManifestFile(const ManifestFile&) = delete;
+  ManifestFile &operator=(const ManifestFile&) = delete;
+
+  [[nodiscard]] const Glib::RefPtr<Gio::File> &path() const
+    {
+      return m_path;
+    }
+  [[nodiscard]] Gio::File &file() const
+    {
+      return *m_path;
+    }
+private:
+  Glib::RefPtr<Gio::File> m_path;
+};
+
+}
+}
+
